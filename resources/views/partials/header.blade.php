e819cd781921af83c2a0edd8bd5162a0e18f4f75
iLBigga <Giannanti1996@gmail.com>
2022-10-18 16:58:34 +0200

    Aggiunto layouts

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="{{ route('students.index') }}">Home</a>
+            </li>
+            <li>
+                <a href="{{ route('students.index') }}">Studenti</a>
+            </li>
+        </ul>
+    </nav>
+</header>
